f49f10f24012c0b730b6d1e0e4d08cecdbaa7bd7
anonymous <email@example.com>
2023-05-08 18:09:09 +1000

    delete disk

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@
 module.exports = {
   //基本路径
   publicPath: "./",
-  outputDir: "dist", //构建时输出目录 默认dist
+  outputDir: "docs", //构建时输出目录 默认dist
   //放置静态资源目录
   assetsDir: "static",
   indexPath: "index.html", //html输出路径 默认 index.html
